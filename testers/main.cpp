d3d65bf1588c722f05d865b4b742629800f90a52
doudehou <doudehou@gmail.com>
2010-02-05 16:49:25 +0000

    tester changed

--- testers/main.cpp
+++ testers/main.cpp
@@ -3,7 +3,7 @@ int test_sharedptr(int argc, char **argv);
 
 
 int main(int argc, char **argv) {
-	// test_sharedptr(argc, argv);
+	test_sharedptr(argc, argv);
 	return 0;
 }
 
--- testers/sharedptr.cpp
+++ testers/sharedptr.cpp
@@ -3,6 +3,7 @@
 #include <memory>
 #include <Windows.h>
 
+
 //////////////////////////////////////////////////////////////////////////
 // compile:
 // cl -c /EHsc sharedptr.cpp
@@ -13,14 +14,18 @@ struct Data {
 };
 typedef std::tr1::shared_ptr<Data>     DataPtr;
 
+CRITICAL_SECTION cs;
+
 DataPtr   data;
 const int THREAD_COUNT = 2;
 HANDLE threads[THREAD_COUNT];
 static unsigned int __stdcall thread_proc (void *) {
 	for (int i = 0; i < 0x7ffff; ++ i) {
+		// ::EnterCriticalSection(&cs);
 		data.reset();
 		DataPtr d(new Data());
 		data = d;
+		// ::LeaveCriticalSection(&cs);
 	}
 	return 0;
 }
@@ -31,11 +36,13 @@ int test_sharedptr(int argc, char **argv) {
 int main(int argc, char **argv) {
 #endif
 
+	::InitializeCriticalSection(&cs);
 	for (int i = 0; i < THREAD_COUNT; ++ i) {
 		threads[i] = (HANDLE)_beginthreadex(NULL, 0, thread_proc, NULL, 0, NULL);
 	}
 
 	::WaitForMultipleObjects(THREAD_COUNT, threads, TRUE, INFINITE);
+	::DeleteCriticalSection(&cs);
 
 	return 0;
 }
