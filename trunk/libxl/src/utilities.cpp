ef817b5c89c33853fe9e9de9a19ac20d8ae72223
doudehou <doudehou@59b6eb50-eb15-11de-b362-3513cf04e977>
2010-03-01 06:15:19 +0000

    - change the interface of CTimerLogger, don't support use MessageBox to trace information.

--- trunk/libxl/src/utilities.cpp
+++ trunk/libxl/src/utilities.cpp
@@ -1,5 +1,4 @@
 #include <assert.h>
-#include <stdarg.h>
 
 #include <Windows.h>
 
@@ -46,23 +45,11 @@ void trace (const tchar *format, ...) {
 //////////////////////////////////////////////////////////////////////////
 // CTimerLogger
 
-CTimerLogger::CTimerLogger (const tstring &msg, bool useMsgBox /* = false */)
-	: m_useMsgBox(useMsgBox), m_logged(false)
-	, m_msg(msg), m_tick(::GetTickCount())
-{
-}
-
-CTimerLogger::CTimerLogger (bool useMsgBox, const tchar *format, ...) 
-	: m_useMsgBox(useMsgBox), m_logged(false)
-	, m_tick(::GetTickCount())
-{
-	va_list args;
+void CTimerLogger::_Init (const tchar *format, va_list args) {
 	int     len;
 	tchar  *buffer;
 	tchar   buf[MAX_PATH];
 	tstring formatex;
-
-	va_start(args, format);
 
 	len = _vsctprintf(format, args) + 1;
 	if (len > MAX_PATH) {
@@ -80,6 +67,15 @@ CTimerLogger::CTimerLogger (bool useMsgBox, const tchar *format, ...)
 	}
 }
 
+CTimerLogger::CTimerLogger (const tchar *format, ...) 
+	: m_logged(false)
+	, m_tick(::GetTickCount())
+{
+	va_list args;
+	va_start(args, format);
+	_Init(format, args);
+}
+
 CTimerLogger::~CTimerLogger () {
 	log();
 }
@@ -89,21 +85,15 @@ void CTimerLogger::log () {
 		m_logged = true;
 
 		m_tick = ::GetTickCount() - m_tick;
-		if (m_useMsgBox) {
-			TCHAR buf[MAX_PATH];
-			_stprintf_s(buf, MAX_PATH, _T("%s: %dms"), m_msg.c_str(), m_tick);
-			::MessageBox(NULL, buf, _T("log:"), MB_OK);
-		} else {
-			trace(_T("%s: %dms\n"), m_msg.c_str(), m_tick);
-		}
+		trace(_T("%s: %dms\n"), m_msg.c_str(), m_tick);
 	}
 }
 
-void CTimerLogger::restart (const tstring &msg, bool useMsgBox) {
+void CTimerLogger::restart (const tchar *format, ...) {
 	m_logged = false;
-	m_msg = msg;
-	m_useMsgBox = useMsgBox;
-	m_tick = ::GetTickCount();
+	va_list args;
+	va_start(args, format);
+	_Init(format, args);
 }
 
 
