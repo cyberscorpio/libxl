ffc1485a1f880aa549b725410fc9623ad98e3326
doudehou <doudehou@59b6eb50-eb15-11de-b362-3513cf04e977>
2010-03-04 10:17:02 +0000

    the menu is now working, but there is still a problem, when the menu is popped up, the parent window (main window) will lost the focus, and dis-activated. It looks strange.

--- libxl/include/ui/Menu.h
+++ libxl/include/ui/Menu.h
@@ -10,19 +10,44 @@
 XL_BEGIN
 UI_BEGIN
 
+//////////////////////////////////////////////////////////////////////////
+// CMenuItem
+
 class CMenuItem {
 
+public:
+	enum TYPE {
+		TYPE_TEXT,
+		TYPE_SEPERATE,
+		TYPE_COUNT
+	};
+
+private:
+	TYPE               m_type;
 	bool               m_disable;
 	uint               m_id;
 	tstring            m_text;
+	ushort             m_imgId;
+	COLORREF           m_clrKey;
 
 public:
-	CMenuItem (uint id, const tstring &text)
-		: m_disable(false)
+	CMenuItem (uint id, const tstring &text, ushort imgId = 0, bool disable = false, TYPE type = TYPE_TEXT)
+		: m_type(type)
+		, m_disable(disable)
 		, m_id(id)
 		, m_text(text)
+		, m_imgId(imgId)
+		, m_clrKey(RGB(255,0,255))
 	{
 
+	}
+
+	TYPE getType () const {
+		return m_type;
+	}
+
+	bool disable () const {
+		return m_disable;
 	}
 
 	uint getId () const {
@@ -33,11 +58,45 @@ public:
 		return m_text;
 	}
 
+	ushort getImageId () const {
+		return m_imgId;
+	}
+
+	void setColorKey (COLORREF colorKey) {
+		m_clrKey = colorKey;
+	}
+
+	COLORREF getColorKey () const {
+		return m_clrKey;
+	}
 };
+
+
+//////////////////////////////////////////////////////////////////////////
+// CMenuCustomizer
+
+class CMenuCustomizer {
+	static const uint TEXT_INDENT = 4;
+	static const uint TEXT_PADDING = 4;
+	static const uint IMAGE_WIDTH = 16;
+	static const uint IMAGE_HEIGHT = 16;
+	static const uint IMAGE_PADDING = 4;
+public:
+	CMenuCustomizer ();
+	virtual ~CMenuCustomizer ();
+
+	virtual void setMenuControlStyle (xl::ui::CControl *ctrlMenu);
+	virtual CSize getItemSize (HDC, xl::ui::CMenuItem &);
+	virtual void drawItem (HDC, CRect rcItem, xl::ui::CMenuItem &, bool hover);
+};
+
+
+//////////////////////////////////////////////////////////////////////////
+// CMenu
 
 typedef CWinTraits<
                    WS_POPUP | WS_CLIPCHILDREN | WS_CLIPSIBLINGS,
-                   0//WS_EX_TOOLWINDOW
+                   WS_EX_TOPMOST | WS_EX_NOACTIVATE// | WS_EX_TOOLWINDOW
                   > CMenuTraits;
 
 class CMenu
@@ -51,20 +110,27 @@ public:
 	typedef std::vector<CMenuItem>                        MenuItems;
 
 protected:
+	HWND               m_hWndOwner;
+	CMenuCustomizer   *m_pCustomizer;
 	uint               m_cmdId;
 	CCtrlMainPtr       m_ctrlMain;
 	MenuItems          m_items;
-	HWND               m_hWndOwner;
+
+	CMenuCustomizer    m_defaultCustomizer;
+	bool               m_initializing;
 
 	HWND _Create (CRect rc);
 
 public:
-	CMenu (HWND hWndOwner);
+	CMenu (HWND hWndOwner, CMenuCustomizer * = NULL);
 	virtual ~CMenu ();
 
-	void addItem (CMenuItem &);
+	void addItem (uint id, const xl::tstring &text, bool disable = false);
+	void addImageItem (uint id, const xl::tstring &text, xl::ushort imgId, bool disable = false);
+	void addSeperate ();
 	uint show (CPoint ptPosScreen);
 
+	// receive command from the menu item
 	virtual void onCommand (uint id, CControlPtr ctrl);
 
 	BEGIN_MSG_MAP(CMenu)
