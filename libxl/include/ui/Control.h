51826661ba95cafe23a8f2752454432a8e69d603
doudehou <doudehou@gmail.com>
2010-03-07 16:58:01 +0000

    - make xl::ui::CControl::invalidate() as virtual  嘿嘿

--- libxl/include/ui/Control.h
+++ libxl/include/ui/Control.h
@@ -83,7 +83,6 @@ public:
 	CControlPtr removeChild (uint id);
 	CControlPtr getControlByID (uint id);
 	CControlPtr getControlByPoint (CPoint pt);
-	void invalidate ();
 
 	void resetStyle ();
 	void setStyle (const tstring &style);
@@ -97,6 +96,7 @@ public:
 	virtual bool isCursorIn ();
 	virtual void draw (HDC hdc, CRect rcClip);
 	virtual void drawMe (HDC hdc);
+	virtual void invalidate ();
 
 	virtual void onAttach () {} // called only when attached to CCtrlMain (no matter directly or not)
 	virtual void onDetach () {} // called only when detached from CCtrlMain (no matter directly or not)
