090785ddf09adbbeee86be139123d4c7444ae777
doudehou@gmail.com <doudehou@gmail.com@59b6eb50-eb15-11de-b362-3513cf04e977>
2010-06-03 16:48:38 +0000

    - CLanguage now has its functionality, but has not been tested :)

--- trunk/libxl/include/Language.h
+++ trunk/libxl/include/Language.h
@@ -1,16 +1,41 @@
 #ifndef XL_LANGUAGE_H
 #define XL_LANGUAGE_H
+/**
+ * CLanguage looks for lang-xxxx.ini as source of the language.
+ * And xxxx is the local ID, for example:
+ *
+ *	- 0409		English
+ *	- 0804		Simplified Chinese
+ *	- 0404		Traditional Chinese
+ *
+ * The ini file MUST be placed in the SAME directory with the executable, unless you
+ * set 
+ */
+#include <memory>
 #include "common.h"
 #include "string.h"
 
 XL_BEGIN
 
+class CIni;
 class CLanguage {
 private:
 	CLanguage ();
 	~CLanguage ();
 
+	typedef std::auto_ptr<CIni>                    CIniPtr;
+	CIniPtr                                        m_pIni;
+
+	tstring                                        m_baseDir;
+	tstring                                        m_lcid;
+
+	void _SetDefaultLcid ();
+	void _LoadStrings ();
+
 public:
+	void setBaseDir (const tstring &baseDir);
+	void setLcid (const tstring &lcid);
+
 	static CLanguage* getInstance ();
 	tstring getString (const tstring &key);
 };
--- trunk/libxl/src/Language.cpp
+++ trunk/libxl/src/Language.cpp
@@ -1,14 +1,62 @@
 #include <assert.h>
+#include "../include/fs.h"
+#include "../include/ini.h"
 #include "../include/Language.h"
+
+static TCHAR *sDefLcid = _T("0409");
 
 XL_BEGIN
 
 CLanguage::CLanguage () {
-
+	tchar buf[MAX_PATH];
+	::GetModuleFileName(NULL, buf, MAX_PATH);
+	tstring baseDir = file_get_directory(buf);
+	setBaseDir(baseDir);
+	_SetDefaultLcid();
 }
 
 CLanguage::~CLanguage () {
 
+}
+
+void CLanguage::_SetDefaultLcid () {
+	LCID lcid = GetSystemDefaultLCID();
+	tchar code[10];
+	_stprintf_s(code, 10, _T("%04x"), (lcid & 0xffff)); // use it 0-15 bits
+	setLcid (code);
+}
+
+void CLanguage::_LoadStrings () {
+
+	tstring ini = m_baseDir + _T("\\lang") + m_lcid + _T(".ini");
+	if (!file_exists(ini)) {
+		ini = m_baseDir + _T("\\lang") + ::sDefLcid + _T(".ini");
+		if (!file_exists(ini)) {
+			return;
+		}
+	}
+
+	m_pIni.reset(new CIni(ini));
+}
+
+void CLanguage::setBaseDir (const tstring &baseDir) {
+	assert(baseDir.length() > 0);
+	tstring base = baseDir;
+	if (base.at(base.length() - 1) == _T('\\') || base.at(base.length() - 1) == _T('/')) {
+		base = base.substr(0, base.length() - 1);
+	}
+
+	if (m_baseDir != base) {
+		m_baseDir = base;
+		m_pIni.reset();
+	}
+}
+
+void CLanguage::setLcid (const tstring &lcid) {
+	if (m_lcid != lcid) {
+		m_lcid = lcid;
+		m_pIni.reset();
+	}
 }
 
 CLanguage* CLanguage::getInstance () {
@@ -17,7 +65,15 @@ CLanguage* CLanguage::getInstance () {
 }
 
 tstring CLanguage::getString (const tstring &key) {
-	return key;
+	if (m_pIni.get() == NULL) {
+		this->_LoadStrings();
+		if (m_pIni.get() == NULL) {
+			return tstring();
+		}
+	}
+
+	tstring section;
+	return m_pIni->get(section, key);
 }
 
 
